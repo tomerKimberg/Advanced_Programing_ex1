f12911e32597dc2b4b6252f536285772564dbdc2
tomerKimberg <tomerkimberg@gmail.com>
2022-11-12 18:13:15 +0200

    vector functions header

---
+++ vector_functions.h
@@ -0,0 +1,19 @@
+//
+// Created by tomer on 12/11/2022.
+//
+
+#ifndef TARGIL1_VECTOR_FUNCTIONS_H
+#define TARGIL1_VECTOR_FUNCTIONS_H
+#include <iostream>
+#include <vector>
+
+std::vector<double> vectorSum(const std::vector<double> v1, const std::vector<double> v2);
+std::vector<double> vectorMax(const std::vector<double> v1, const std::vector<double> v2);
+std::vector<double> vectorPow(const std::vector<double> v1, const std::vector<double> v2);
+std::vector<double> vectorSubtraction(const std::vector<double> v1, const std::vector<double> v2);
+std::vector<double> vectorAbs(const std::vector<double> v1, const std::vector<double> v2);
+std::vector<double> vectorAddition(const std::vector<double> v1, const std::vector<double> v2);
+std::vector<double> vectorDivision(const std::vector<double> v1, const std::vector<double> v2);
+
+
+#endif //TARGIL1_VECTOR_FUNCTIONS_H
