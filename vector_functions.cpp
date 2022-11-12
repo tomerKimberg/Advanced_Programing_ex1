f1148c88ccb2a0fb5ac36047f3e1ccaaa22c5582
tomerKimberg <tomerkimberg@gmail.com>
2022-11-12 21:44:55 +0200

    vector functions cpp and header files finished

--- vector_functions.cpp
+++ vector_functions.cpp
@@ -84,3 +84,34 @@ std::vector<double> vectorSubtraction(const std::vector<double> v1, const std::v
     }
     return resVector;
 }
+
+/**
+ *
+ * @param v1 vector given my user
+ * @param v2 vector given my user
+ * @return vector data, where every element in index i is the absolute value of that element at index i in v1
+ */
+std::vector<double> vectorAbs(const std::vector<double> v1){
+    std::vector<double> resVector;
+    for(double d : v1){
+        resVector.push_back(abs(d));
+    }
+    return resVector;
+}
+/**
+ *
+ * @param v1 vector given my user
+ * @param v2 vector given my user
+ * @return vector data, where every element in index i is the division of x[i] of v1 from y[i] of v2
+ */
+std::vector<double> vectorDivision(const std::vector<double> v1, const std::vector<double> v2){
+    std::vector<double> resVector;
+    if(v1.size() == v2.size()) {
+        for (int index = 0; index < v1.size(); index +=1){
+            resVector.push_back(v1.at(index) / v2.at(index));
+        }
+        return resVector;
+
+    }
+    return resVector;
+}
--- vector_functions.h
+++ vector_functions.h
@@ -11,7 +11,7 @@ double vectorSum(const std::vector<double> v1);
 double vectorMax(const std::vector<double> v1);
 std::vector<double> vectorPow(const std::vector<double> v1, double power);
 std::vector<double> vectorSubtraction(const std::vector<double> v1, const std::vector<double> v2);
-std::vector<double> vectorAbs(const std::vector<double> v1, const std::vector<double> v2);
+std::vector<double> vectorAbs(const std::vector<double> v1);
 std::vector<double> vectorAddition(const std::vector<double> v1, const std::vector<double> v2);
 std::vector<double> vectorDivision(const std::vector<double> v1, const std::vector<double> v2);
 
