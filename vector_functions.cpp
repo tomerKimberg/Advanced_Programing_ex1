3507d57fa84e842f97b2fc71b48e5ffdfabb08c3
dstr951 <dstr159@gmail.com>
2022-11-12 18:56:43 +0200

    implement vectorSum function

--- vector_functions.cpp
+++ vector_functions.cpp
@@ -2,3 +2,18 @@
 #include <vector>
 #include <math.h>
 #include "vector_functions.h"
+
+/*
+input: vector<double>
+output: double
+this function returns a sum from all of the values in the vector
+*/
+double vectorSum(const std::vector<double> v1)
+{
+    double sum = 0;
+    for(double d: v1)
+    {
+        sum += d;
+    }
+    return sum;
+}
