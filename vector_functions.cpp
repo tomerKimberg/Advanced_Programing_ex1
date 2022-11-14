68272575de75c52fe83fb2e68ed524b6b80608cc
tomerKimberg <tomerkimberg@gmail.com>
2022-11-14 20:37:55 +0200

    removed exceptions, added vector validation function. updated vector functions accordingly

--- vector_functions.cpp
+++ vector_functions.cpp
@@ -2,17 +2,13 @@
 #include <vector>
 #include <math.h>
 #include "vector_functions.h"
-#include "vector_function_exeption.h"
 
 /**
  *
  * @param v1  vector<double>
  * @return double. this function returns a sum from all of the values in the vector
  */
-double vectorSum(const std::vector<double> v1) throw(vector_function_exeption){
-    if(v1.empty()){
-        throw vector_function_exeption();
-    }
+double vectorSum(const std::vector<double> v1){
     double sum = 0;
     for (double d: v1) {
         sum += d;
@@ -25,16 +21,14 @@ double vectorSum(const std::vector<double> v1) throw(vector_function_exeption){
  * @param v1 vector<double>
  * @return double, this function returns the maximum value in the vector
  */
-double vectorMax(const std::vector<double> v1) throw(vector_function_exeption){
-    if(v1.empty()){
-        throw vector_function_exeption();        
-    }
+double vectorMax(const std::vector<double> v1){
     double max = v1[0];
     for (double d: v1) {
         if (d > max) {
             max = d;
         }
-    }    
+    }
+    return max;
 }
 
 /**
@@ -44,10 +38,7 @@ double vectorMax(const std::vector<double> v1) throw(vector_function_exeption){
  * @return vector<double, this function returns a new vector,
  * such that every value is a value from v1 to the power of the double passed as a function parameter
  */
-std::vector<double> vectorPow(const std::vector<double> v1, double power)throw(vector_function_exeption){
-    if(v1.empty()) {
-        throw vector_function_exeption();
-    }
+std::vector<double> vectorPow(const std::vector<double> v1, double power){
     std::vector<double> newVector;
     for (double d: v1) {
         newVector.push_back(pow(d, power));
@@ -62,18 +53,11 @@ std::vector<double> vectorPow(const std::vector<double> v1, double power)throw(v
  * @param v2 vector given my user
  * @return vector data, where every element in index i is the sum of those elements in vectors v1 and v2
  */
-std::vector<double> vectorAddition(const std::vector<double> v1, const std::vector<double> v2)
-throw(vector_function_exeption){
-    if(v1.empty()) {
-        throw vector_function_exeption();
-    }
-    if(v1.size() != v2.size()) {
-        //throw vector_different_lenghts_exception
-    }
+std::vector<double> vectorAddition(const std::vector<double> v1, const std::vector<double> v2){
     std::vector<double> resVector;
     for (int index = 0; index < v1.size(); index +=1){
         resVector.push_back(v1.at(index) + v2.at(index));
-    }
+        }
     return resVector;
 }
 /**
@@ -82,14 +66,7 @@ throw(vector_function_exeption){
  * @param v2 vector given my user
  * @return vector data, where every element in index i is the subtraction of x[i] of v1 from y[i] of v2
  */
-std::vector<double> vectorSubtraction(const std::vector<double> v1, const std::vector<double> v2)
-throw(vector_function_exeption){
-    if(v1.empty()) {
-        throw vector_function_exeption();
-    }
-    if(v1.size() == v2.size()) {
-        //throw vector_different_lenghts_exception
-    }
+std::vector<double> vectorSubtraction(const std::vector<double> v1, const std::vector<double> v2){
     std::vector<double> resVector;
     for (int index = 0; index < v1.size(); index +=1){
         resVector.push_back(v1.at(index) - v2.at(index));
@@ -103,10 +80,7 @@ throw(vector_function_exeption){
  * @param v2 vector given my user
  * @return vector data, where every element in index i is the absolute value of that element at index i in v1
  */
-std::vector<double> vectorAbs(const std::vector<double> v1) throw(vector_function_exeption){
-    if(v1.empty()) {
-        throw vector_function_exeption();
-    }
+std::vector<double> vectorAbs(const std::vector<double> v1) {
     std::vector<double> resVector;
     for (double d: v1) {
         resVector.push_back(abs(d));
@@ -118,15 +92,8 @@ std::vector<double> vectorAbs(const std::vector<double> v1) throw(vector_functio
  * @param v1 vector given my user
  * @param v2 vector given my user
  * @return vector data, where every element in index i is the division of x[i] of v1 from y[i] of v2.
- * if the vectors are not the same length, an custom exception is thrown.
  */
-std::vector<double> vectorDivision(const std::vector<double> v1, const std::vector<double> v2) throw(std::exception){
-    if(v1.empty()) {
-        throw vector_function_exeption();
-    }
-    if(v1.size() == v2.size()) {
-        //throw vector_different_lenghts_exception
-    }
+std::vector<double> vectorDivision(const std::vector<double> v1, const std::vector<double> v2){
     std::vector<double> resVector;
     for (int index = 0; index < v1.size(); index +=1){
         if(v1.at(index) == 0) {
