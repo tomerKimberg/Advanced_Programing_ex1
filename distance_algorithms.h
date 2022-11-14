4e0a9b257e6d1e4f18a9162a8b9167dec393499c
tomerKimberg <tomerkimberg@gmail.com>
2022-11-14 20:52:51 +0200

    .cpp and .h for the distance algorithms + the implimitation of the Canberra distance algorithms

---
+++ distance_algorithms.h
@@ -0,0 +1,9 @@
+#ifndef TARGIL1_DISTANCE_ALGORITHMS_H
+#define TARGIL1_DISTANCE_ALGORITHMS_H
+
+#include <iostream>
+#include <vector>
+#include "vector_functions.h"
+
+double Canberra_distane(const std::vector<double> v1, const std::vector<double> v2);
+#endif //TARGIL1_DISTANCE_ALGORITHMS_H
