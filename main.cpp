9649794cc457f8c6bf35b6603cc9f1ee2cd9b4a2
dstr951 <dstr159@gmail.com>
2022-11-15 23:12:52 +0200

    split the getVectorFromInput to two functions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <sstream>
 #include <regex>
 #include "vector_functions.h"
+#define number_of_vectors 2
+#define BAD_INPUT_MESSAGE "You entered an invalid input, please try to run the program again."
 
 /**
  *
@@ -14,15 +16,13 @@ bool input_valid(std::string line)
     const std::regex input_regex("^(?:\\s|\\d|(?:\\d\\.\\d)|(?:\\-\\d)|(?:\\-\\d.\\d))+$");
     return std::regex_match(line, input_regex);
 }
-/*
-input: none
-output: vector<double>
-this function reads a line of the following format: "1.3 2 3 4 5" and creates a vector from it
-*/
-std::vector<double> getVectorFromInput()
+/**
+ *
+ * @param std::string line
+ * @return std::vector<double>, creates a new vector from the string
+ */
+std::vector<double> vectorFromString(std::string line)
 {
-    std::string line = "";
-    std::getline(std::cin, line);
     std::stringstream stringStream;
     stringStream.str(line);
     double inputValue = 0;
@@ -46,12 +46,26 @@ void printVector(const std::vector<double> vectorToPrint)
     }
     std::cout << std::endl;
 }
+/**
+ * takes two lines of input, creates vectors from them and prints the distance between the vectors in 5 different ways
+ */
+void measureDistance(){
+    std::string line = "";
+    std::vector<double> inputVectors[2];
+    for(int i = 0; i < number_of_vectors; i++){
+        std::getline(std::cin, line);
+        if (!input_valid(line)){
+            std::cout << BAD_INPUT_MESSAGE << std::endl;
+            return;
+        }
+        inputVectors[i] = vectorFromString(line);
+    }
+    printVector(inputVectors[0]);
+    printVector(inputVectors[1]);
+    printVector(vectorAddition(inputVectors[0], inputVectors[1]));
+}
 
 int main()
 {
-    std::vector<double> firstVector = getVectorFromInput();
-    std::vector<double> secondVector = getVectorFromInput();
-    printVector(firstVector);
-    printVector(secondVector);
-    printVector(vectorAddition(firstVector,secondVector));
+    measureDistance();
 }
