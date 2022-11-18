d7bb4652e299cf4a1f2c61c6b11b84cffe5a6ef1
dstr951 <dstr159@gmail.com>
2022-11-18 15:58:35 +0200

    create new files

---
+++ vector_validation.cpp
@@ -0,0 +1,28 @@
+
+#include <vector>
+#include <string>
+#include <regex>
+#include "vector_validation.h"
+/**
+ *
+ * @param std::string line
+ * @return bool, true if the string is a valid vector string or false if it's not.
+ */
+bool input_valid(std::string line)
+{
+    const std::regex input_regex("^(?:\\s|\\d|(?:\\d\\.\\d)|(?:\\-\\d)|(?:\\-\\d.\\d))+$");
+    return std::regex_match(line, input_regex);
+}
+/**
+ * make sure the vectors are at same length and are not empty
+ * @param v1
+ * @param v2
+ * @return true if vectors are valid, else false
+ */
+bool vector_validation(const std::vector<double>& v1,  const std::vector<double>& v2){
+    if(v1.capacity() == v2.capacity() && !v1.empty()){
+        return true;
+    }
+    return false;
+
+}
