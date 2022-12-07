796a0c908738435e7b6ff180e84ffb4bc5287422
tomerKimberg <tomerkimberg@gmail.com>
2022-12-07 17:10:03 +0200

    create header and cpp file for DataExtractor and FileExtractor classes

---
+++ DataExtractor.h
@@ -0,0 +1,16 @@
+//
+// Created by tomer on 07/12/2022.
+//
+
+#ifndef TARGIL1_DATAEXTRACTOR_H
+#define TARGIL1_DATAEXTRACTOR_H
+#include <iostream>
+
+class DataExtractor {
+public:
+    virtual std::string getData() = 0;
+    virtual bool hasNext() = 0;
+};
+
+
+#endif //TARGIL1_DATAEXTRACTOR_H
---
+++ FileExtractor.cpp
@@ -0,0 +1,16 @@
+
+#include "FileExtractor.h"
+
+FileExtractor::FileExtractor(std::string path) {}
+std::string FileExtractor::getPath() {
+    return std::string();
+}
+void FileExtractor::setPath(std::string newPath) {
+    this->path = newPath;
+}
+std::string FileExtractor::getData() {}
+bool FileExtractor::hasNext() {}
+
+
+
+
