94f6bbc761dc7c45ce0561c97b39ddf8aca41634
tomerKimberg <tomerkimberg@gmail.com>
2022-12-07 23:09:01 +0200

    added description to copy() to make sure this functions purpose is known

--- DataExtractor.h
+++ DataExtractor.h
@@ -9,6 +9,11 @@
 class DataExtractor {
 public:
     virtual ~DataExtractor() = 0;
+    /**
+ * create a new concrete DataExtractor object from one of its derivative classes
+ * on the Heap and returns it's address! must save pointer to delete later!
+ * @return DataExtractor pointer
+ */
     virtual DataExtractor* copy() = 0;
     virtual std::string getData() = 0;
     virtual bool hasNext() = 0;
