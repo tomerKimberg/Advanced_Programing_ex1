35aeac8d0e618aa32e2efa0b7a62615f381fd2b8
tomerKimberg <tomerkimberg@gmail.com>
2022-12-07 17:18:39 +0200

    create header for all headers of the base and derivative classes of DataExtractor

---
+++ headerDataExtractors.h
@@ -0,0 +1,9 @@
+//
+// Created by tomer on 07/12/2022.
+//
+
+#ifndef TARGIL1_HEADERDATAEXTRACTORS_H
+#define TARGIL1_HEADERDATAEXTRACTORS_H
+#include "DataExtractor.h"
+#include "FileExtractor.h"
+#endif //TARGIL1_HEADERDATAEXTRACTORS_H
