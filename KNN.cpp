0c5279cab549ddf97bc437478a892d3c71b53cbb
tomerKimberg <tomerkimberg@gmail.com>
2022-12-08 20:03:25 +0200

    added documentation to code and fixed the code a bit

--- KNN.cpp
+++ KNN.cpp
@@ -6,7 +6,12 @@ KNN::KNN(std::map<std::vector<double>, std::vector<std::string>> neighbors,
          std::string metric, int k) {
     this->neighbors = neighbors;
     this->distanceCalculatorMetric = this->FDC.createDistanceCalculator(metric);
-    this->k = k;
+    if(k >= 0) {
+        this->k = k;
+    }
+    else{
+        this->k = 0;
+    }
     this->metric = metric;
 }
 
@@ -22,8 +27,8 @@ void KNN::setK(int k) {
 }
 
 void KNN::setMetric(std::string metric) {
-    //delete old distance calculator
-    delete this->distanceCalculatorMetric;
+
+    //delete this->distanceCalculatorMetric;
     this->distanceCalculatorMetric = this->FDC.createDistanceCalculator(metric);
 }
 
@@ -54,10 +59,9 @@ std::vector<std::vector<double>> KNN::getKNearestNeighbors(std::vector<double> v
 
 std::string KNN::getClassification(std::vector<double> v1) {
     this->run(v1);
-    std::vector<std::vector<std::string>> KNearestClassifications = this->getProcessedClassification();
+    std::vector<std::string> KNearestClassifications = this->getProcessedClassification();
     std::map<std::string, int> classifications;
-    for (std::vector<std::string> stringVector: KNearestClassifications) {
-        for (std::string classification: stringVector) {
+        for (std::string classification: KNearestClassifications) {
             std::map<std::string, int>::iterator it = classifications.find(classification);
             if (it != classifications.end()) {
                 it->second += 1;
@@ -65,7 +69,7 @@ std::string KNN::getClassification(std::vector<double> v1) {
                 classifications.insert({classification, 1});
             }
         }
-    }
+
     int max = 0;
     std::string classification;
     for (std::map<std::string, int>::iterator iter = classifications.begin(); iter != classifications.end(); ++iter) {
--- KNN.h
+++ KNN.h
@@ -16,31 +16,56 @@ private:
     distanceCalculator* distanceCalculatorMetric;
     std::map<std::vector<double>, std::vector<std::string>> neighbors;
     factoryDistanceCalculator FDC;
-    // add container attribute to store distances
+    /**
+     * inner class used to store a distance and the associated data with it
+     */
     class distanceAndData{
     private:
         double distance;
+        /**
+         * inner class to store the vector<double> and vector<string> together
+         */
         class neighbor{
         private:
             std::vector<double>* vectorDouble;
             std::vector<std::string>* classification;
         public:
+            /**
+             * constructor
+             */
             neighbor(){
                 this->vectorDouble = nullptr;
                 this->classification = nullptr;
             }
+            /**
+             * constructor
+             * @param vectorDouble pointer to vector<double>
+             * @param classification pointer to vector<vector<string>>
+             */
             neighbor(std::vector<double>* vectorDouble, std::vector<std::string>* classification){
                 this->vectorDouble = vectorDouble;
                 this->classification = classification;
             }
             //implement copy constructor
-
+            /**
+             * getter
+             * @return pointer to vector<double>
+             */
             std::vector<double>* getVectorDouble(){
                 return this->vectorDouble;
             }
+            /**
+             * getter
+             * @return pointer to vector<vector<string>>
+             */
             std::vector<std::string>* getVectorClassification(){
                 return this->classification;
             }
+            /**
+             * prameter overloading for comparison of neighbor class
+             * @param n2 anoth neighbor object
+             * @return true if objects are equal, else false.
+             */
             bool operator== (neighbor n2)
             {
                 return (*this->getVectorDouble() == *n2.getVectorDouble()
@@ -49,15 +74,30 @@ private:
 
         };
         neighbor data;
+
     public:
+        /**
+         * constructor
+         * @param d  calculated distance value
+         * @param vectorDouble the vector<double> that the distance was calculated to
+         * @param classification it's classification
+         */
         distanceAndData(double d,  std::vector<double>* vectorDouble,
                         std::vector<std::string>* classification){
             this->distance = d;
             this->data = neighbor(vectorDouble,classification);
         }
+        /**
+         * getter, a const function, doesn't change objects values
+         * @return  double, the distance
+         */
         double getDistance() const {
             return this->distance;
         }
+        /**
+         * return the neighbor object that stores pointers to both vector<double> and vector<vector<string>>
+         * @return
+         */
         neighbor getData(){
             return this->data;
         }
@@ -69,19 +109,32 @@ private:
 
 
     };
+
     //vector of distance and data object that stores a computed distance and a dataVector associated with it
     std::vector<distanceAndData> processedData;
 
     //private functions-
+    /**
+     * update the processedData with new information
+     * @param distance double
+     * @param vectorDouble
+     * @param classification
+     */
     void updateProcessedData(double distance, std::vector<double>* vectorDouble,
                              std::vector<std::string>* classification);
-
+    /**
+     * for all the data inside the neighbors map, calculate distance and update the processedData accordingly
+     * @param v1 the vector to get its k-nearest neighbors
+     */
     void run(std::vector<double> v1){
+        //iterate over all neighbors in the map
         for(std::map<std::vector<double>, std::vector<std::string>>::iterator iter = this->neighbors.begin();
             iter != this->neighbors.end(); ++iter)
         {
             std::vector<double> v2 =  iter->first;
+            //compute distance from v1 for every v2
             double distance = this->distanceCalculatorMetric->calculateDistance(v1,v2);
+            //update the processedData
             this->updateProcessedData(distance, const_cast<std::vector<double> *>(&(iter->first)), &iter->second);
         }
 
@@ -89,6 +142,10 @@ private:
         std::nth_element(this->processedData.begin(), (this->processedData.begin() + (this->k-1)),
                 this->processedData.end());
     }
+    /**
+     * get k nearest vectors<double>
+     * @return vector of vector<double -> vector<vector<double>>
+     */
     std::vector<std::vector<double>> getProcessedVectorData(){
         std::vector<std::vector<double>> res;
         int k = 1;
@@ -103,14 +160,18 @@ private:
         }
         return res;
     }
-    std::vector<std::vector<std::string>> getProcessedClassification(){
-        std::vector<std::vector<std::string>> res;
+    /**
+     * get k nearest Classifications
+     * @return vector<string>
+     */
+    std::vector<std::string> getProcessedClassification(){
+        std::vector<std::string> res;
         int k = 1;
         for(distanceAndData temp : this->processedData){
+            for(std::string classification : *temp.getData().getVectorClassification())
             if(k <= this->k) {
                 k+=1;
-                const std::vector<std::string>& temp1 = *temp.getData().getVectorClassification();
-                res.push_back(temp1);
+                res.push_back(classification);
             }
             else{
                 break;
@@ -121,20 +182,49 @@ private:
 
 
 public:
+    /**
+     * constructor KNN
+     * @param neighbors map<key=vector<double>, value=vector<string>>
+     * @param metric string that represents a metric method for the algorithm to use
+     * @param k int, number of closest neighbors to look for
+     */
     KNN(std::map<std::vector<double>, std::vector<std::string>> neighbors,
         std::string metric, int k);
+    //implement, first decide on factory implementation
     ~KNN();
-
+    /**
+     * set new neighbor map
+     * @param neighbors map<key=vector<double>, value=vector<string>>
+     */
     void setNeighbors( std::map<std::vector<double>, std::vector<std::string>> neighbors);
+    /**
+     * set new k nearest neighbors
+     * @param k
+     */
     void setK(int k);
-    //make sure to delete former distance calculator
+    //make sure to delete former distance calculator if needed (depends on factory implementation)
+    /**
+     * set a new metric function
+     * @param metric  string name of metric method
+     */
     void setMetric(std::string metric);
-
+    /**
+     * getter
+     * @return int k
+     */
     int getK();
     std::map<std::vector<double>, std::vector<std::string>> getNeighbors();
-
+    /**
+     * get the classification cording to the k nearest neighbors
+     * @param v1 vector to get classification for
+     * @return string name of classification
+     */
     std::string getClassification(std::vector<double> v1);
-    //might change based on design
+    /**
+     * get the k nearest neighbors
+     * @param v1 vector to get its k nearest neighbors
+     * @return vector<vector<double>>
+     */
     std::vector<std::vector<double>> getKNearestNeighbors(std::vector<double> v1);
 
 };
