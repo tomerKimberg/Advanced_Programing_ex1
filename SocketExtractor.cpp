998139cd03739acc4e60bf09f30a138f4850d3a1
dstr951 <dstr159@gmail.com>
2022-12-23 19:31:40 +0200

    create hasNext function

--- SocketExtractor.cpp
+++ SocketExtractor.cpp
@@ -1,1 +1,13 @@
+#include <sys/ioctl.h>
 #include "SocketExtractor.h"
+
+#define SOCKET_EXTRACTOR_DEBUG 0
+
+bool SocketExtractor::hasNext(){
+    int bytesLeft = 0;
+    ioctl(this->fileDescriptor, FIONREAD, &bytesLeft);
+    if(SOCKET_EXTRACTOR_DEBUG){
+        std::cout << "bytes left: " << bytesLeft;
+    }
+    return bytesLeft > 0;
+}
