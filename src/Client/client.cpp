7ee0906d90eb5b83ff0a01e76f29d77d3c28931e
tomerKimberg <tomerkimberg@gmail.com>
2022-12-26 20:14:36 +0200

    added validation check for user input in client
    moved code from main to outer function

--- src/Client/client.cpp
+++ src/Client/client.cpp
@@ -7,25 +7,45 @@
 #include "../Extractors/DataExtractor.h"
 #include "..//Extractors/FileExtractor.h"
 #include "..//SocketConnection/SocketConnection.h"
+#include "../ValidationFuncs/user_input.h"
 
+
+void run(SocketConnection client);
 
 int main(){
     SocketConnection client(12345);
+    run(client);
+}
+
+void run(SocketConnection client) {
     bool connection = false;
-    if(client.connect() > 0){
+    if(client.connect() == 0){
         connection = true;
     }
 
     while(connection){
         std::string userInput;
-        std::cin >> userInput;
-        if(client.send(userInput)){
-            std::cout <<"message was sent" <<std::endl;
+        getline(std::cin, userInput);
+        if(validUserInput(splitUserInput(userInput)) || userInput == "-1") {
+            if (client.send(userInput)) {
+                //successfully sent close signal to server
+                if(userInput == "-1"){
+                    break;
+
+                }
+                //add code to receive and print result from server
+                std::cout << "message was sent" << std::endl;
+            }
+                //there was an error sending to the server, probably connection lost
+            else {
+                connection = false;
+            }
         }
         else{
-            connection = false;
+            std::cout << "input invalid" << std::endl;
         }
 
     }
     client.close();
+
 }
