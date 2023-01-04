1c8f538039255232e6de1bca52327123fe735087
dstr951 <dstr159@gmail.com>
2023-01-04 20:46:11 +0200

    use arguments in client

--- src/Client/client.cpp
+++ src/Client/client.cpp
@@ -11,10 +11,24 @@
 #include "..//SocketConnection/SocketConnection.h"
 #include "../ValidationFuncs/user_input.h"
 
+#define CLIENT_NUMBER_OF_ARGUMENTS 3
+#define CLIENT_ARGS_VARIABLE_PORT 2
+#define CLIENT_ARGS_VARIABLE_IP 1
 
+/**
+ * @param int argc, amount of the program arguments
+ * @param char** argv - the arguments values
+ * @return bool - wether the arguments are valid or not
+*/
+bool validArgs(int argc, char** argv);
 void run(SocketConnection client);
 
-int main(){
+int main(int argc, char** argv){
+    //check program arguments
+    if(!validArgs(argc, argv)){
+        std::cout << ARGS_BAD_MESSAGE << std::endl;
+        return 1;
+    }
     SocketConnection client(12345);
     run(client);
 }
@@ -50,4 +64,19 @@ void run(SocketConnection client) {
     }
     client.close();
 
+}
+bool validArgs(int argc, char** argv){
+    if(CLIENT_NUMBER_OF_ARGUMENTS != argc){
+        std::cout << BAD_ARGUMENTS_NUMBER << std:: endl;
+        return false;
+    }
+    if(!isIP(argv[CLIENT_ARGS_VARIABLE_IP])){
+        std::cout << BAD_ARGUMENT_IP_MESSAGE << std:: endl;
+        return false;
+    }
+    if(!isPort(argv[CLIENT_ARGS_VARIABLE_PORT])){
+        std::cout << BAD_ARGUMENT_PORT_MESSAGE << std:: endl;
+        return false;
+    }
+    return true;
 }
