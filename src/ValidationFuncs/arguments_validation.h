f97fa9c2839eb48f55c79b9d10d9f35dfb84b66f
dstr951 <dstr159@gmail.com>
2023-01-04 10:00:20 +0200

    fix note

--- src/ValidationFuncs/arguments_validation.h
+++ src/ValidationFuncs/arguments_validation.h
@@ -2,7 +2,7 @@
 #define TARGIL1_ARGUMENTS_VALIDATION_H
 #include <string>
 /**
- * This function tries to convert the given string and returns a boolean value according to the result
+ * This function tries to convert the given string to ipv4 address and returns a boolean value according to the result
  * @param std::string ip the ip we want to convert
  * @return bool
 */
