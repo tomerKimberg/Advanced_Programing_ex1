6f57feae9909fd50fab480674c22e07391817bc4
dstr951 <dstr159@gmail.com>
2023-01-04 08:44:12 +0200

    create header file for validation functions

---
+++ src/ValidationFuncs/arguments_validation.h
@@ -0,0 +1,6 @@
+#ifndef TARGIL1_ARGUMENTS_VALIDATION_H
+#define TARGIL1_ARGUMENTS_VALIDATION_H
+#include <string>
+bool isIP(std::string ip);
+bool isPort(std::string port);
+#endif //TARGIL1_ARGUMENTS_VALIDATION_H
