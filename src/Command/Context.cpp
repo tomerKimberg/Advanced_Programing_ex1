e7dc9873a292e9507686ed85b31b3a4d53747a7c
tomerKimberg <tomerkimberg@gmail.com>
2023-01-19 12:20:37 +0200

    context- k,metric,getneighbor

--- src/Command/Context.cpp
+++ src/Command/Context.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "Context.h"
+
+Context::Context(){
+    this->k = new int;
+    *(this->k) = 5;
+    //this->metric = "AUC";
+    this->metric = "AUC";
+    this->gn = nullptr;
+    this->knn = nullptr;
+    this->result = nullptr;
+}
+Context::~Context() {
+    delete this->k;
+    delete this->gn;
+    delete this->knn;
+
+
+}
+
+int *Context::getK() const {
+    return k;
+}
+
+KNN *Context::getKnn() const {
+    return knn;
+}
+
+GetNeighbors *Context::getGn() const {
+    return gn;
+}
+
+const std::string &Context::getMetric() const {
+    return metric;
+}
+
+std::vector<int, std::string> *Context::getResult() const {
+    return result;
+}
+
+void Context::setK(int k) {
+    *(Context::k) = k;
+}
+
+void Context::setKnn() {
+    if(this->knn){
+        delete this->knn;
+        this->knn = new KNN(this->gn->getNeighborsInMap(), this->metric, *this->k);
+    }
+    else{
+        this->knn = new KNN(this->gn->getNeighborsInMap(), this->metric, *this->k);
+    }
+}
+
+void Context::setGn(DataExtractor& extractor) {
+    if(this->gn){
+        delete this->gn;
+        this->gn = new GetNeighbors(&extractor);
+    }
+    else{
+        this->gn = new GetNeighbors(&extractor);
+    }
+}
+
+void Context::setMetric(const std::string &metric) {
+    Context::metric = metric;
+}
+
+void Context::setResult(std::vector<int, std::string> *result) {
+    Context::result = result;
+}
--- src/Command/Context.h
+++ src/Command/Context.h
@@ -13,9 +13,31 @@ private:
   int* k;
   KNN* knn;
   GetNeighbors* gn;
-  std::string* metric;
+  std::string metric;
   std::vector<int,std::string>* result;
+public:
+    Context();
+    ~Context();
 
+    int *getK() const;
+
+    KNN *getKnn() const;
+
+    GetNeighbors *getGn() const;
+
+    const std::string &getMetric() const;
+
+    std::vector<int, std::string> *getResult() const;
+
+    void setK(int k);
+
+    void setKnn();
+
+    void setGn(DataExtractor& extractor);
+
+    void setMetric(const std::string &metric);
+
+    void setResult(std::vector<int, std::string> *result);
 };
 
 
