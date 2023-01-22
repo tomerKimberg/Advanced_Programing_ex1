6fc7e1a11f9c2c1c71183440e2093f13fa0027c6
dstr951 <dstr159@gmail.com>
2023-01-22 20:03:44 +0200

    use constant in server response

--- src/CommunicationProtocol.h
+++ src/CommunicationProtocol.h
@@ -4,6 +4,7 @@
 #define INVALID_MESSAGE_PATH "invalid path\n"
 #define INVALID_MESSAGE_VALUE_K "invalid value for K\n"
 #define INVALID_MESSAGE_VALUE_METRIC "invalid value for metric\n"
+#define INVALID_MESSAGE_MENU_OPTION "invalid menu option, please try again.\n"
 #define ERROR_MESSAGE_REQUIRED_CLASSIFICATION "please classify the data\n"
 #define ERROR_MESSAGE_REQUIRED_DATA "please upload data\n"
 
