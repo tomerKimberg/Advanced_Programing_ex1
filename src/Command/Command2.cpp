771d65996faa29d98fdd4694e853978502fc5936
dstr951 <dstr159@gmail.com>
2023-01-24 11:14:53 +0200

    implement communication dor command 2

--- src/Command/Command2.cpp
+++ src/Command/Command2.cpp
@@ -6,32 +6,43 @@
 #include "Command2.h"
 #include "../CommunicationProtocol.h"
 #include "../ValidationFuncs/vector_validation.h"
+
+#define COMMAND_2_DEBUG 0
+
 Command2::Command2(std::string description, DefaultIO *io, Context *context) : Command(description, io, context) {
     this->kBackup = *(this->context->getK());
     this->metricBackUp = this->context->getMetric();
 }
 
 void Command2::execute() {
-    std::string message = "The current KNN parameters are: K = " + std::to_string(this->kBackup) +
+    std::string currentKAndMetric = "The current KNN parameters are: K = " + std::to_string(this->kBackup) +
             ", distance metric = " + this->metricBackUp + "\n";
-    this->io->write(message);
+    this->io->write(currentKAndMetric);
     //expect to get number and metric
-    std::stringstream ss(this->io->read());
+    std::string response = this->io->read();
+    if(COMMAND_2_DEBUG){
+        std::cout << "response from client is: " << response << std::endl;
+    }
+    if(COMMUNICATION_MESSAGE_RECEIVED == response){
+        return;
+    }
+    std::stringstream ss(response);
     bool valid  = true;
 
     int kTemp;
     std::string metricTemp;
     std::string kString;
+    std::string errors = "";
     ss >> kString;
     ss >> metricTemp;
 
-    int len = kString.size();
-    if(len == 0){
-        return;
+    if(COMMAND_2_DEBUG){
+        std::cout << "kString is: " << kString << " metricTemp is: " << metricTemp << std::endl;
     }
+    int len = kString.size();
     for(int i = 0 ; i < len; i++){
         if(!std::isdigit(kString[i])) {
-            this->io->write(INVALID_MESSAGE_VALUE_K);
+            errors += INVALID_MESSAGE_VALUE_K;
             valid = false;
             break;
         }
@@ -42,10 +53,12 @@ void Command2::execute() {
 
 
     if(!checkMetric(metricTemp)){
-        this->io->write(INVALID_MESSAGE_VALUE_METRIC);
+        errors += INVALID_MESSAGE_VALUE_METRIC;
         valid = false;
     }
     if(valid){
+        //send ok message to client because the client is expecting an error message
+        this->io->write(COMMUNICATION_MESSAGE_RECEIVED);
         this->context->setK(kTemp);
         this->context->setMetric(metricTemp);
         //the new parameters are different from the current ones
@@ -54,5 +67,9 @@ void Command2::execute() {
         }
         this->kBackup = kTemp;
         this->metricBackUp = metricTemp;
+    }else{
+        this->io->write(errors);
     }
+    //read ok message before printing menu
+        this->io->read();
 }
